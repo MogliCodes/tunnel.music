91a267fe9eedd3aed827f61cb87d88a0d7f8f257
MogliCodes <moglicodes@gmail.com>
2023-03-05 12:17:26 +0100

    fix: set navigation item background color only for dark mode

--- components/app/AppNavigation.vue
+++ components/app/AppNavigation.vue
@@ -1,9 +1,9 @@
 <template>
   <nav>
     <ul>
-      <li class="mr-2 inline-block bg-gray-900"><nuxt-link class="block px-4 py-2" to="/albums">albums</nuxt-link></li>
-      <li class="mr-2 inline-block bg-gray-900"><nuxt-link class="block px-4 py-2" to="/playlists">playlists</nuxt-link></li>
-      <li class="inline-block bg-gray-900"><nuxt-link class="block px-4 py-2" to="/about">about</nuxt-link></li>
+      <li class="mr-2 inline-block dark:bg-gray-900"><nuxt-link class="block px-4 py-2" to="/albums">albums</nuxt-link></li>
+      <li class="mr-2 inline-block dark:bg-gray-900"><nuxt-link class="block px-4 py-2" to="/playlists">playlists</nuxt-link></li>
+      <li class="inline-block dark:bg-gray-900"><nuxt-link class="block px-4 py-2" to="/about">about</nuxt-link></li>
     </ul>
   </nav>
 </template>
