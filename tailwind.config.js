9aafe0b5676dfa42912e0b86ad3064d2d97bcebd
MogliCodes <moglicodes@gmail.com>
2023-04-15 15:51:19 +0200

    chore: clean up project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./pages/**/*.{html,js}', './components/**/*.{html,js}'],
-  safelist: ['grid-cols-6'],
+  safelist: [
+    'grid-cols-2',
+    'grid-cols-3',
+    'grid-cols-4',
+    'grid-cols-5',
+    'grid-cols-6',
+    'grid-cols-7',
+    'grid-cols-8',
+    'grid-cols-9',
+  ],
   theme: {
     fontFamily: {
       mono: ['IBM Plex Mono'],
