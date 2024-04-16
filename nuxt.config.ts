89c16e91597bed6a66a8e5a1375db084e322d229
MogliCodes <moglicodes@gmail.com>
2024-04-16 18:37:21 +0200

    chore(deps): update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true,
+  // ssr: true,
   app: {
     head: {
       script: [{src: 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r70/three.min.js'}]
@@ -25,12 +25,12 @@ export default defineNuxtConfig({
       clientDb: true
     }
   },
-  nitro: {
-    prerender: {
-      // https://github.com/nuxt-themes/docus/issues/944#issuecomment-1634580369
-      concurrency: 1,
-      // https://github.com/nuxt-themes/docus/issues/944#issuecomment-1634798275
-      failOnError: false,
-    },
-  },
+  // nitro: {
+  //   prerender: {
+  //     // https://github.com/nuxt-themes/docus/issues/944#issuecomment-1634580369
+  //     concurrency: 1,
+  //     // https://github.com/nuxt-themes/docus/issues/944#issuecomment-1634798275
+  //     failOnError: false,
+  //   },
+  // },
 })
